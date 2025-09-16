4b809b84ad415da2ff0afe270b5f16db9f2406c7
SHIN.C. <sch04265@gmail.com>
2025-09-16 23:20:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 76.47 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i = 0; i < ingredient.length; i++) {
+            stack.push(ingredient[i]);
+
+            if(stack.size() >= 4) {
+                int size = stack.size();
+                if(stack.get(size-4) == 1 && 
+                   stack.get(size-3) == 2 && 
+                   stack.get(size-2) == 3 && 
+                   stack.get(size-1) == 1) {
+
+                    for(int j = 0; j < 4; j++) {
+                        stack.pop();
+                    }
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
