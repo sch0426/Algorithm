16fb0c14c9422e5919025644129371b53ff612aa
SHIN.C. <sch04265@gmail.com>
2025-11-21 17:24:24 +0900

    [Silver IV] Title: 괄호, Time: 108 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int tc = 0; tc < T; tc++) {
+            String PS = br.readLine();
+            Deque<Character> stack = new ArrayDeque<>();
+            boolean isOk = true;
+
+            for(int i = 0; i < PS.length(); i++) {
+                char c = PS.charAt(i);
+                if(c == '(') {
+                    stack.push(c);
+                } else {
+                    if(stack.isEmpty()) {
+                        isOk = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            if(isOk && stack.isEmpty()) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
