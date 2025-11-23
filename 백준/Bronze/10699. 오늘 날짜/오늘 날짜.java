58fb0cac6a4fa84725dac9c1d071e55de1982ef6
SHIN.C. <sch04265@gmail.com>
2025-11-23 21:42:02 +0900

    [Bronze V] Title: 오늘 날짜, Time: 152 ms, Memory: 16736 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now(ZoneId.of("Asia/Seoul"));
+        System.out.print(localDate);
+    }
+}
