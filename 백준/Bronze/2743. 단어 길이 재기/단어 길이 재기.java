3e3f0fadadbadf68cceb7e5695b33255c59c7aa9
SHIN.C. <sch04265@gmail.com>
2025-11-28 14:27:45 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(String.valueOf(br.readLine().length()));
+        bw.close();
+    }
+}
