f7224b8379c211af7d46971e847ba44ebb7b6e19
SHIN.C. <sch04265@gmail.com>
2025-11-28 15:21:42 +0900

    [Bronze V] Title: 이상한 기호, Time: 112 ms, Memory: 14132 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int answer = (A + B) * (A - B);
+
+        bw.write(String.valueOf(answer));
+        bw.close();
+    }
+}
