825df15aad3181cbe72c50008280a4b8f610e5c8
SHIN.C. <sch04265@gmail.com>
2025-12-02 12:29:32 +0900

    [Bronze V] Title: CASIO, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        if(a - b == 0) sb.append(1);
+        else sb.append(0);
+
+        bw.write(sb.toString());
+        bw.close();
+    }
+}
