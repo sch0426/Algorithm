9238fc9958dd82c11b0997f858d6ef57d9c6af11
SHIN.C. <sch04265@gmail.com>
2025-12-03 10:45:42 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 14120 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int answer = 0;
+
+        for(int i = 0; i < 5; i++) {
+            answer += Integer.parseInt(br.readLine());
+        }
+        System.out.println(answer);
+    }
+}
