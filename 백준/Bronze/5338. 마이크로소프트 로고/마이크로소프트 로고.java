56076e76152330b82b7497b28616479b17c4e86a
SHIN.C. <sch04265@gmail.com>
2025-12-03 10:42:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        //       _.-;;-._
+        //'-..-'|   ||   |
+        //'-..-'|_.-;;-._|
+        //'-..-'|   ||   |
+        //'-..-'|_.-''-._|
+        sb.append("       _.-;;-._").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-;;-._|").append("\n");
+        sb.append("'-..-'|   ||   |").append("\n");
+        sb.append("'-..-'|_.-''-._|");
+        System.out.println(sb.toString());
+    }
+}
