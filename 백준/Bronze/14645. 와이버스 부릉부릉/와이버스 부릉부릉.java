3e2e9b4d0008ddff52ed18c1fdb207c2fe6644a7
SHIN.C. <sch04265@gmail.com>
2025-12-03 13:48:44 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 92 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+        }
+        System.out.println("비와이");
+    }
+}
