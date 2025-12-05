edb50e0121f5a30e83a37cfa7b16f300e7d83647
SHIN.C. <sch04265@gmail.com>
2025-12-05 16:55:05 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        //  ___  ___  ___
+        //  | |__| |__| |
+        //  |           |
+        //   \_________/
+        //    \_______/
+        //     |     |
+        //     |     |
+        //     |     |
+        //     |     |
+        //     |_____|
+        //  __/       \__
+        // /             \
+        ///_______________\
+
+        sb.append("  ___  ___  ___").append("\n");
+        sb.append("  | |__| |__| |").append("\n");
+        sb.append("  |           |").append("\n");
+        sb.append("   \\_________/").append("\n");
+        sb.append("    \\_______/").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |     |").append("\n");
+        sb.append("     |_____|").append("\n");
+        sb.append("  __/       \\__").append("\n");
+        sb.append(" /             \\").append("\n");
+        sb.append("/_______________\\").append("\n");
+
+        System.out.println(sb.toString());
+    }
+}
