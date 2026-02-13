fc237ce361b4c0f920ce716044e6d934de88348c
sch0426 <sch04265@gmail.com>
2026-02-13 13:32:01 +0900

    [Gold IV] Title: 최단경로, Time: 832 ms, Memory: 111204 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,67 +2,76 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Node {
-    int e, w;
-
-    Node(int e, int w) {
-        this.e = e;
-        this.w = w;
-    }
-}
-
+/*
+ * 방향그래프가 주어지면 주어진 시작점에서 다른 모든 정점으로의 최단 경로를 구하는 프로그램을 작성하시오. 
+ * 단, 모든 간선의 가중치는 10 이하의 자연수이다.
+ * 첫째 줄에 정점의 개수 V와 간선의 개수 E가 주어진다. (1 ≤ V ≤ 20,000, 1 ≤ E ≤ 300,000)
+ */
 public class Main {
-    static int V, E, K;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        V = Integer.parseInt(st.nextToken()); // 정점의 개수
-        E = Integer.parseInt(st.nextToken()); // 간선의 개수
-        K = Integer.parseInt(br.readLine()); // 시작 정점의 번호
-        ArrayList<Node>[] adj = new ArrayList[V + 1];
-        // 정점의 개수만큼 배열 선언 , 인덱스 1부터 시작할거임
-        for(int i = 1; i < V + 1; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        // 간선의 개수만큼 입력이 주어진다
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            adj[Integer.parseInt(st.nextToken())].add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
 
-        int[] dist = new int[V + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        boolean[] visited = new boolean[V + 1];
-        dist[K] = 0;
+	static class Node {
+		int v, w;
 
-        for(int step = 1; step < V + 1; step++) {
-            // 거리배열에서 기준정점으로 방문하지 않은 정점중 최소거리정점을 선택한다
-            int minIdx = -1;
-            int minDist = Integer.MAX_VALUE;
-            for(int i = 1; i < V + 1; i++) {
-                if(!visited[i] && dist[i] < minDist) {
-                    minIdx = i;
-                    minDist = dist[i];
-                }
-            }
+		public Node(int v, int w) {
+			super();
+			this.v = v;
+			this.w = w;
+		}
+		
+	}
+	
+	static int V, E, dist[];
+	static ArrayList<Node>[] graph;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(br.readLine()) - 1;
+		graph = new ArrayList[V];
+		for(int i = 0; i < V; i++) {
+			graph[i] = new ArrayList<>();
+		}
+		dist = new int[V];
+		for(int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken()) - 1;
+			int v = Integer.parseInt(st.nextToken()) - 1;
+			int w = Integer.parseInt(st.nextToken());
+			graph[u].add(new Node(v, w));
+		}
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dijkstra(start);
+		for (int distance : dist) {
+			System.out.println(distance == Integer.MAX_VALUE ? "INF" : distance);
+		}
+	}
+	
+	private static void dijkstra(int start) {
+		PriorityQueue<Node> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.w, b.w));
+		
+		dist[start] = 0;
+		pq.add(new Node(start, 0));
+		
+		while(!pq.isEmpty()) {
+			Node curr = pq.poll();
+			int v = curr.v;
+			int w = curr.w;
+			
+			if(dist[v] < w) continue;
+			
+			for(Node node : graph[v]) {
+				if(dist[node.v] > dist[v] + node.w) {
+					dist[node.v] = dist[v] + node.w;
+					pq.add(new Node(node.v, dist[node.v]));
+				}
+			}
+		}
+		
+	}
 
-            if(minIdx == -1) break;
-
-            // 더이상 볼 필요없다
-            visited[minIdx] = true;
-            for(Node node : adj[minIdx]) {
-                if(dist[node.e] > dist[minIdx] + node.w) {
-                    dist[node.e] = dist[minIdx] + node.w;
-                }
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < V + 1; i++) {
-            sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append("\n");
-        }
-        System.out.println(sb);
-    }
 }
