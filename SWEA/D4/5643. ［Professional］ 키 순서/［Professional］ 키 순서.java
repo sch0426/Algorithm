82543c9671682390d867b3c137bb73ed09f7fa18
sch0426 <sch04265@gmail.com>
2026-04-14 16:05:23 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,555 ms, Memory: 109,604 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -1,67 +1,53 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+// SWEA 5643. 키 순서
 public class Solution {
-    static int N, M, smallStu[], bigStu[];
-    static ArrayList<Integer>[] student;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+	static final int INF = 100000000;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+			
+			int[][] adj = new int[N + 1][N + 1];
+			for(int i = 1; i <= N; i++) {
+				Arrays.fill(adj[i], INF);
+				adj[i][i] = 0;
+			}
+			
+			for(int i = 0; i < M; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				adj[start][end] = 1;
+			}
+			
+			for (int k = 1; k <= N; k++) {
+				for (int i = 1; i <= N; i++) {
+					for (int j = 1; j <= N; j++) {
+						if(adj[i][k] == INF || adj[k][j] == INF) continue;
+						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
+					}
+				}
+			}
+			
+			int cnt = 0;
+			for(int i = 1; i <= N; i++) {
+				int temp = 0;
+				for(int j = 1; j <= N; j++) {
+					if(i == j) continue;
+					if(adj[i][j] != INF || adj[j][i] != INF) temp++;
+				}
+				if(temp == N - 1) cnt++;
+			}
+			
+			sb.append("#" + tc + " ").append(cnt).append("\n");
+		}
+		System.out.println(sb);
+	}
 
-        for(int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine()); // 학생 수
-            M = Integer.parseInt(br.readLine()); // 비교 횟수
-            student = new ArrayList[N + 1];
-            for(int i = 1; i <= N; i++) {
-                student[i] = new ArrayList<>();
-            }
-            for(int i = 0; i < M; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                int smallStuNum = Integer.parseInt(st.nextToken());
-                int bigStuNum = Integer.parseInt(st.nextToken());
-                student[smallStuNum].add(bigStuNum); // 작은 학생이 큰 학생 바라보도록 리스트 연결
-            }
-
-            smallStu = new int[N + 1];
-            bigStu = new int[N + 1];
-            for(int i = 1; i <= N; i++) {
-                bfs(i);
-            }
-
-            int ans = 0;
-            for(int i = 1; i <= N; i++) {
-                if(smallStu[i] + bigStu[i] == N - 1) ans++;
-            }
-            sb.append("#" + tc + " ").append(ans).append("\n");
-        }
-        System.out.println(sb);
-
-    }
-
-    private static void bfs(int startStu) {
-        Deque<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[N + 1];
-        visited[startStu] = true;
-        q.offer(startStu);
-
-        while(!q.isEmpty()) {
-            int curr = q.poll();
-
-            for(int next : student[curr]) {
-                if(!visited[next]) {
-                    visited[next] = true;
-                    q.offer(next);
-
-                    bigStu[startStu]++;
-                    smallStu[next]++;
-                }
-            }
-        }
-
-    }
 }
