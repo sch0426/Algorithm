587483d929f33430867637c40a8394d6419ee1ba
sch0426 <sch04265@gmail.com>
2026-04-14 15:29:21 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,113 ms, Memory: 103,264 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -5,11 +5,11 @@ import java.util.*;
 
 // SWEA 1263. 사람 네트워크2
 public class Solution {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		final int INF = 100000000;
 		
 		for(int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,21 +17,15 @@ public class Solution {
 			int[][] adj = new int[N][N];
 			
 			for(int i = 0; i < N; i++) {
-				Arrays.fill(adj[i], Integer.MAX_VALUE);
-			}
-			
-			for(int i = 0; i < N; i++) {
 				for(int j = 0; j < N; j++) {
-					int tmp = Integer.parseInt(st.nextToken());
-					if(tmp == 1) adj[i][j] = tmp;
+					adj[i][j] = Integer.parseInt(st.nextToken());
+					if(adj[i][j] == 0) adj[i][j] = INF;  
 				}
-				adj[i][i] = 0;
 			}
 			
 			for (int k = 0; k < N; k++) {
 				for (int i = 0; i < N; i++) {
 					for (int j = 0; j < N; j++) {
-						if(adj[i][k] == Integer.MAX_VALUE || adj[k][j] == Integer.MAX_VALUE) continue;
 						adj[i][j] = Math.min(adj[i][j], adj[i][k] + adj[k][j]);
 					}
 				}
@@ -42,6 +36,7 @@ public class Solution {
 			for (int i = 0; i < N; i++) {
 				int currSum = 0;
 				for (int j = 0; j < N; j++) {
+					if(i == j) continue;
 					currSum += adj[i][j];
 				}
 				sum = Math.min(sum, currSum);
