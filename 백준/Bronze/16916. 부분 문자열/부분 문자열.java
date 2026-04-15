2e985f9c3d5bf93b4c068d92bfd112f813100c08
sch0426 <sch04265@gmail.com>
2026-04-15 14:51:00 +0900

    [Bronze II] Title: 부분 문자열, Time: 280 ms, Memory: 23004 KB -BaekjoonHub

--- 백준/Bronze/16916. 부분 문자열/부분 문자열.java
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.java
@@ -2,54 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String S = br.readLine();
 		String P = br.readLine();
-		
-		int[] pi = getPi(P);
-		
-		System.out.println(search(S, P, pi));
-	}
-
-	private static int[] getPi(String P) {
-		int[] pi = new int[P.length()];
-		int j = 0;
-		
-		for(int i = 1; i < P.length(); i++) {
-			while(j > 0 && P.charAt(i) != P.charAt(j)) {
-				j = pi[j - 1];
-			}
-			
-			if(P.charAt(i) == P.charAt(j)) {
-				pi[i] = ++j;
-			}
-		}
-		
-		return pi;
-	}
-	
-	private static int search(String S, String P, int[] pi) {
 		int n = S.length();
 		int m = P.length();
-		int j = 0;
 		
-		for(int i = 0; i < n; i++) {
-			while(j > 0 && S.charAt(i) != P.charAt(j)) {
-				j = pi[j - 1];
+		if(n < m) {
+			System.out.println(0);
+			return;
+		}
+		
+		long mod = 1_000_000_007;
+		long hashKey = 31;
+		
+		long pHashVal = 0;
+		long sHashVal = 0;
+		long power = 1;
+		
+		for(int i = 0; i < m; i++) {
+			sHashVal = (sHashVal * hashKey + S.charAt(i)) % mod;
+			pHashVal = (pHashVal * hashKey + P.charAt(i)) % mod;
+			if(i < m - 1) power = (power * hashKey) % mod;
+		}
+		
+		for(int i = 0; i <= n - m; i++) {
+			if(pHashVal == sHashVal) {
+				if(S.substring(i, i + m).equals(P)) {
+					System.out.println(1);
+					return;
+				}
 			}
 			
-			if(S.charAt(i) == P.charAt(j)) {
-				if(j == m - 1) {
-					return 1;
-				} else {
-					j++;
-				}
+			if(i < n - m) {
+				sHashVal = (hashKey * (sHashVal - S.charAt(i) * power) + S.charAt(i + m)) % mod;
+				
+				if(sHashVal < 0) sHashVal += mod;
 			}
 		}
 		
-		return 0;
+		System.out.println(0);
 	}
-	
+
 }
