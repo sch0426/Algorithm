dccc01861c0cabbbac2881a3f7e4cd4060d8807e
sch0426 <sch04265@gmail.com>
2026-05-01 16:06:12 +0900

    [level 3] Title: 단어 변환, Time: 0.54 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,54 +1,49 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
-        String str;
-        int weight;
+    class Node {
+        String word;
+        int cnt;
         
-        Node (String str, int weight) {
-            this.str = str;
-            this.weight = weight;
+        public Node(String word, int cnt) {
+            this.word = word;
+            this.cnt = cnt;
         }
     }
-    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         
-        boolean flag = false;
-        for (String word : words) {
-            if (word.equals(target)) {
-                flag = true;
-                break;
-            }
-        }
-        
-        if (!flag) return 0;
-        
-        Queue<Node> q = new ArrayDeque<>();
-        Map<String, Boolean> m = new HashMap<>();
+        Deque<Node> q = new ArrayDeque<>();
         q.offer(new Node(begin, 0));
+        boolean[] v = new boolean[words.length];
         
-        while (!q.isEmpty()) {
-            Node now = q.poll();
+        while(!q.isEmpty()) {
+            Node curr = q.poll();
             
-            if (now.str.equals(target)) {
-                answer = now.weight;
-                break;
+            if(curr.word.equals(target)) {
+                return curr.cnt;
             }
             
-           
-            for (String word : words) {
-                int count = 0;
-                for (int i = 0; i < word.length(); i++) {
-                    if (word.charAt(i) != now.str.charAt(i)) count++;
-                }
-                if (count == 1 && !m.containsKey(word)) {
-                    q.offer(new Node(word, now.weight + 1));
-                    m.put(word, true);
+            for(int i = 0; i < words.length; i++) {
+                if(!v[i] && canChange(curr.word, words[i])) {
+                    v[i] = true;
+                    q.offer(new Node(words[i], curr.cnt + 1));
                 }
             }
         }
+        
         
         return answer;
+    }
+    
+    
+    
+    // 두 단어가 하나만 다르면 return true
+    private boolean canChange(String s1, String s2) {
+        int cnt = 0;
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) != s2.charAt(i)) cnt++;
+        }
+        return cnt == 1;
     }
 }
